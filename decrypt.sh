553ceb875ff1f163009ba072fe9f00ca757182bc
m3rc1ful <13939411+m3rc1ful@users.noreply.github.com>
2023-01-08 16:03:22 +0200

    Update decrypt.sh

--- decrypt.sh
+++ decrypt.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 
-## Goto subdir where CURRENTLY ENCRYPTED Data is stored
-cd Data
-
-## Delete CURRENTLY ENCRYPTED content of this Data directory
-rm -rf *
-
-## Copy ORIGINAL UNENCRYPTED content to this Data directory
-cp -r --no-preserve=links ../OriginalData/* ./
-
 ## Delete CURRENTLY ENCRYPTED content of S3 bucket
 aws s3 rm --recursive s3://platinum-sec-blood-private/poc1/
 
+## Goto subdir where ORIGINAL UNENCRYPTED OriginalData is backed-up
+cd OriginalData
+
 ## Upload ORIGINAL UNENCRYPTED files to s3 bucket
 aws s3 cp --recursive ./ s3://platinum-sec-blood-private/poc1/
+
+## Go up one directory and delete locally-stored OriginalData dir
+cd ..
+rm -rfv OriginalData
